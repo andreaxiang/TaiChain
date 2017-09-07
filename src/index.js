0001fe9cbcfce4f79a253c9c61f919068b2e0192
andreaxiang <xiangxue89@163.com>
2017-09-07 10:15:07 +0800

    引入bootstrap源文件

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import _ from 'lodash'   //引入lodash
-import $ from 'jquery'   //引入jquery
-require('bootstrap')
+import _ from 'lodash';   //引入lodash
+import $ from 'jquery';  //引入jquery
 
 function component() {
-  //var element = document.createElement('div');
-  var element = $('<div></div>')
+  var element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
   //element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  /*element.html(_.join(['Hello','webpack'], ' '))*/
 
-  //return element;
-  return element.get(0);
+  return element;
 }
 
 document.body.appendChild(component());
